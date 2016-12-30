3b3032441c8650a37ddfa39d852dd03c70ace548
thomasquintana <quintana.thomas@gmail.com>
2016-12-30 11:29:14 -0500

    Added doxygen documentation to dsp.h.

--- src/dsp.h
+++ src/dsp.h
@@ -41,27 +41,50 @@ extern "C" {
 #endif
 
 /**
+ * Add two vectors of 64 floats.
  *
+ * @param a The first term.
+ * @param b The second term.
+ * @param c The destination for the sum of a + b.
+ *
+ * @return Void
  */
 void dsp_vec_add_64(float *a, float *b, float *c);
 
 /**
+ * Copy a vector of 16 floats.
  *
+ * @param a The source.
+ * @param b The destination.
+ *
+ * @return Void
  */
 void dsp_vec_copy_16(float *a, float *b);
 
 /**
+ * Multiply two vectors of 64 floats.
  *
+ * @param a The first term.
+ * @param b The second term.
+ * @param c The destination for the product of a * b.
+ *
+ * @return Void
  */
 void dsp_vec_mul_64(float *a, float *b, float *c);
 
 /**
+ * Compute the square root of each float in a vector of 64 floats.
  *
+ * @param a The source.
+ * @param b The destination for the square roots of 64 floats.
  */
 void dsp_vec_sqrt_64(float *a, float *b);
 
 /**
+ * Compute the square of each float in a vector of 64 floats.
  *
+ * @param a The source.
+ * @param b The destination for the square of the 64 floats.
  */
 void dsp_vec_square_64(float *a, float *b);
 
